05bde2e763e88c9e86006f4d896f0dca595387b0
521125123 <3121208981@qq.com>
2020-12-29 20:24:21 +0800

    main

--- wxapp/mall/pages/goods/detail.js
+++ wxapp/mall/pages/goods/detail.js
@@ -6,8 +6,11 @@ Page({
    * 页面的初始数据
    */
   data: {
+    shareQuanziProduct:true,
     goodsDetail: {},
-    goodsId:''
+    goodsId:'',
+    selectSizePrice:'',
+    selectSizeOPrice:''
   },
 
   /**
@@ -26,8 +29,22 @@ Page({
     // goodsDetailRes.data.basicInfo.videoId = '123'
     // console.log(goodsDetailRes)
     this.setData({
-      goodsDetail: goodsDetailRes.data
+      goodsDetail: goodsDetailRes.data,
+      selectSizePrice:goodsDetailRes.data.basicInfo.minPrice,
+      selectSizeOPrice:goodsDetailRes.data.basicInfo.originalPrice,
     })
+  },
+  //小程序值允许分享到个人， 群里， 但是不可以直接分享到朋友圈
+  async drawSharePic(){
+    //用户权限 登陆
+    const qrcodeRes = await WXAPI.wxaQrcode({
+      scene: this.data.goodsDetail.basicInfo.id + ',1111' ,
+      page: 'pages/goods/details',
+      is_hyaline: true,
+      autoColor: true,
+      expireHours: 1
+    })
+    console.log(qrcodeRes)
   },
   onReady: function () {
     console.log('on ready');
