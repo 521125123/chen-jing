36b1d3525bc138ca38fe14a80ce8937819ea77bd
521125123 <3121208981@qq.com>
2020-12-30 14:03:40 +0800

    main

--- wxapp/mall2/pages/goods/list.js
+++ wxapp/mall2/pages/goods/list.js
@@ -1,31 +1,73 @@
 // pages/goods/list.js
+const WXAPI = require('apifm-wxapi')//随机做一个数据，require引入
 Page({
    
   /**
    * 页面的初始数据
    */
   data: {
+    goods:[],
     listType:2,
     name: '', //搜索 做关键字查询
+    list:'list1',
     orderBy: '',//排序条件
+  },
+  bindinput(e){
+    this.setData({
+      name: e.detail.value
+    })
+  },
+  bindconfirm(e){
+    this.setData({
+      name: e.detail.value
+    })
+    this.search()
+  },
+  async search() {
+    wx.showLoading({
+      title: '加载中',
+    })
+    const _data = {
+      orderBy: this.data.orderBy,
+      page: 1,
+      pageSize: 20,
+    }
+    if (this.data.name) {
+      _data.k = this.data.name
+    }
+  // /goods?k=addidas&page=1&pageSize
+    const res = await WXAPI.goods(_data)
+    console.log(res)
+    this.setData({
+      goods:res.data
+    })
+    wx.hideLoading()
   },
   changeShowType(){
     if (this.data.listType == 1) {
       this.setData({
-        listType: 2
+        listType: 2,
+        list:'list1'
       })
     } else {
       this.setData({
-        listType: 1
+        listType: 1,
+        list:'list2'
       })
     }
+  },
+  filter(e){
+    this.setData({
+      orderBy: e.currentTarget.dataset.val
+    })
+    this.search()
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.search();
   },
 
   /**
