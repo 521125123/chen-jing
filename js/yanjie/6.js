967a36bf362a1a773c0029b8aa7db00b5202b3df
521125123 <3121208981@qq.com>
2021-01-06 20:13:17 +0800

    main

--- js/yanjie/6.js
+++ js/yanjie/6.js
@@ -1,4 +1,7 @@
 function duplicateCount(text) {
-    var str = text.toLowerCase().split
+    var str = text.toLowerCase().split('').sort().join('');
+    var arr = str.match(/(-)\1+/g);
+    console.log(str);
+    // var arr = str
 }
 console.log(duplicateCount('aadsdjfwuhh'));
