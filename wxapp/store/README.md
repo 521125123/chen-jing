fe80ab807a07422872f971a7acabbd156eb8ba8b
521125123 <3121208981@qq.com>
2021-01-14 18:01:40 +0800

    main

--- wxapp/store/README.md
+++ wxapp/store/README.md
@@ -14,3 +14,8 @@ isShow true  false 没准备好。
     application <-  page  <-  components  <- 原子的组标
     组件有data 和propertoty的概念，
     page老板  component 打工人 ， property属性  data
+6. tab切换？
+    1. 设置数据  tab状态相关  activeTypeId
+    2. 多个type 放到数据里
+    - css flex  1/4
+    位置怎莫做
--- wxapp/store/miniprogram/pages/homepage/homepage.js
+++ wxapp/store/miniprogram/pages/homepage/homepage.js
@@ -5,14 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
+    fruitInfo: ,
     offline: false, // 打烊
     isShow: true,
     imgUrls: [
       'https://m.360buyimg.com/mobilecms/s843x843_jfs/t1/152869/36/733/129848/5f6da644E8c640b50/35117f17f08456ad.jpg!q70.dpg.webp',
       'https://m.360buyimg.com/mobilecms/s843x843_jfs/t1/149571/38/9289/117895/5f6da644E784f911b/21a284afecffdb4f.jpg!q70.dpg.webp'
+    ],
+    activeTypeId: 0,
+    typeCat: [ /** 类型的数据配置 */
+      {
+        id: 0, name:"美味鲜果",
+      },
+      {
+        id: 1, name: "今日特惠"
+      },
+      {
+        id: 2, name: "新鲜上架"
+      },
+      {
+        id: 3, name: "店主推荐"
+      }
     ]
   },
-
+  typeSwitch(e) {
+    this.setData({
+      activeTypeId: e.currentTarget.dataset.id
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -25,14 +45,16 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+//js与页面进行交互
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+//html 与  css  做数据请求 到云端请求数据 比较早, 放到ready晚了 花时间，  尽量提早
+// onLoad 正在加载 加载且计算渲染页面,线程如果又再启动下载进程的话,可能会比较卡到
+//fruit-bored
   },
 
   /**
