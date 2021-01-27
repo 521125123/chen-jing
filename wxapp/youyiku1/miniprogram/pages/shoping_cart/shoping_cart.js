55b7723449a431ef9686cb27e69c0bc9e1683880
521125123 <3121208981@qq.com>
2021-01-27 19:03:18 +0800

    main

--- wxapp/youyiku1/miniprogram/pages/shoping_cart/shoping_cart.js
+++ wxapp/youyiku1/miniprogram/pages/shoping_cart/shoping_cart.js
@@ -1,19 +1,79 @@
-// miniprogram/pages/shoping_cart/shoping_cart.js
+// miniprogram/pagesoping_cartoping_cart.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    motto:'进行登录操作',
+    iD:1
   },
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+  
+    // (wx.getSetting({
+    //   success: function(res){
+    //    if (res.authSetting['scope.userInfo']) {
+    //     // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+    //     wx.getUserInfo({
+    //      success: function(res) {
+    //       console.log(res.userInfo)
+    //      }
+    //     })
+    //   console.log('success')
+    //    }
+    //   },
+    //   fail:function(res){
+    //     console.log('fail');
+    //     wx.reLaunch({
+    //       url: '/pages/homepage/homepage',
+    //     })
+    //   }
+    //  }))
 
+      var that = this
+    // 判断用户是否授权登录
+      wx.getSetting({
+        success: function (res) {
+          // 判断是否授权
+          console.log('delu')
+          if (res.authSetting['scope.userInfo']) {
+            wx.showToast({
+              title: '已授权',
+            })
+            wx.navigateTo({
+              url: '/pages/login/unme',
+            })
+             //获取用户信息
+             console.log('获取用户信息')
+            wx.getUserInfo({
+              success: function (res) {
+                console.log('用户已经授权过，添加用户信息')
+                //用户已经授权过，添加用户信息
+                var that = this
+                wx.setStorageSync('nickName', res.userInfo.nickName)
+                wx.setStorageSync('avatarUrl', res.userInfo.avatarUrl)
+              }
+            });
+          }else{
+          wx.reLaunch({
+            url: '/pages/login/unme',
+          })
+          }
+        }
+      })
+  },  
+
+
+   bindGetUserInfo: function(e) {
+     console.log(e.detail.userInfo)
   },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
